e95238429ea03c1ae1a9557a4e6aa56a16d4b776
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2026-04-01 15:03:35 -0700

    add profile

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/admin/profile', [ProfileController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/profile/store', [ProfileController::class, 'ProfileStore'])->name('profile.store');
+
+});
+
+require __DIR__.'/auth.php';
